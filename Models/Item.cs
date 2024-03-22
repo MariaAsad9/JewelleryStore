20cd7a594cf4cd387c704131bd7de4828d659841
MariaAsad9 <sp22bscs0045@maju.edu.pk>
2024-03-22 23:24:13 +0500

    Maria Fisrt Commit

---
+++ Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JewelleryStore.Models;
+
+public partial class Item
+{
+    public int Id { get; set; }
+
+    public string? ItemName { get; set; }
+
+    public string? Color { get; set; }
+
+    public string? GemstoneType { get; set; }
+
+    public int? Quantity { get; set; }
+
+    public decimal? Price { get; set; }
+}
---
+++ Models/JewelsContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace JewelleryStore.Models;
+
+public partial class JewelsContext : DbContext
+{
+    public JewelsContext()
+    {
+    }
+
+    public JewelsContext(DbContextOptions<JewelsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-O6KLONO;Database=JEWELS;Trusted_Connection=True;trustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Table__3214EC075C32AC64");
+
+            entity.ToTable("ITEMS");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Color)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.GemstoneType)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.ItemName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("money");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
